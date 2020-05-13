60c905ec024846f187dbe38ad0c10dc9cd7a11d8
Shan Ahmed <21813031@bucks.ac.uk>
2020-05-13 05:57:17 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace Choose_a_Picture
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Choice = new System.Windows.Forms.ComboBox();
+            this.Pictures = new System.Windows.Forms.PictureBox();
+            this.Quit = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.Pictures)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Choice
+            // 
+            this.Choice.FormattingEnabled = true;
+            this.Choice.Items.AddRange(new object[] {
+            "Alien",
+            "Dragon",
+            "Hydra",
+            "Medusa",
+            "Scorpius",
+            "Shadow"});
+            this.Choice.Location = new System.Drawing.Point(76, 112);
+            this.Choice.Name = "Choice";
+            this.Choice.Size = new System.Drawing.Size(177, 24);
+            this.Choice.TabIndex = 0;
+            this.Choice.Text = "Choose here";
+            this.Choice.SelectedIndexChanged += new System.EventHandler(this.Choice_SelectedIndexChanged);
+            // 
+            // Pictures
+            // 
+            this.Pictures.Location = new System.Drawing.Point(452, 66);
+            this.Pictures.Name = "Pictures";
+            this.Pictures.Size = new System.Drawing.Size(269, 259);
+            this.Pictures.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.Pictures.TabIndex = 1;
+            this.Pictures.TabStop = false;
+            this.Pictures.Click += new System.EventHandler(this.Pictures_Click);
+            // 
+            // Quit
+            // 
+            this.Quit.Location = new System.Drawing.Point(624, 358);
+            this.Quit.Name = "Quit";
+            this.Quit.Size = new System.Drawing.Size(97, 36);
+            this.Quit.TabIndex = 2;
+            this.Quit.Text = "Quit";
+            this.Quit.UseVisualStyleBackColor = true;
+            this.Quit.Click += new System.EventHandler(this.Quit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Quit);
+            this.Controls.Add(this.Pictures);
+            this.Controls.Add(this.Choice);
+            this.Name = "Form1";
+            this.Text = "Choose a Picture";
+            ((System.ComponentModel.ISupportInitialize)(this.Pictures)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox Choice;
+        private System.Windows.Forms.PictureBox Pictures;
+        private System.Windows.Forms.Button Quit;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Choose_a_Picture
+{
+    /// <summary>
+    /// IS 3.14 Choose a Picture
+    /// This form uses a picturebox and a combobox to provide the simple function
+    /// of displaying a user chosen image
+    /// Author: Shan Ahmed 21813031
+    /// </summary>
+    /// 
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Choice_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (Choice.Text)
+            {
+                case "alien": Pictures.Image = Image.FromFile("alien.gif"); break;
+                case "Dragon": Pictures.Image = Image.FromFile("dragon.gif"); break;
+                case "Hydra": Pictures.Image = Image.FromFile("hydra.gif"); break;
+                case "Medusa": Pictures.Image = Image.FromFile("medusa.jpg"); break;
+                case "Scorpius": Pictures.Image = Image.FromFile("scorpius.jpg"); break;
+                case "Shadow": Pictures.Image = Image.FromFile("shadow.jpg"); break;
+            }
+            
+          
+        }
+
+        private void Pictures_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Quit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
